9201a19967f0314e3c42585f769d7b2cd8b7aeb2
debris <marek.kotewicz@gmail.com>
2017-08-25 12:40:33 +0200

    tests for log_stream receiving logs

--- tests/tests/log_stream.rs
+++ tests/tests/log_stream.rs
@@ -5,7 +5,7 @@ extern crate bridge;
 extern crate tests;
 
 use std::time::Duration;
-use web3::types::{FilterBuilder, H160, H256};
+use web3::types::{FilterBuilder, H160, H256, Log};
 use bridge::api::{LogStreamInit, log_stream, LogStreamItem};
 
 test_transport_stream! {
@@ -234,4 +234,99 @@ test_transport_stream! {
 	"eth_getLogs" =>
 		req => r#"[{"address":null,"fromBlock":"0x14","limit":null,"toBlock":"0x14","topics":[["0x2222222222222222222222222222222222222222222222222222222222222222"],null,null,null]}]"#,
 		res => r#"[]"#;
+}
+
+test_transport_stream! {
+	name => log_stream_get_log,
+	init => |transport| {
+		let init = LogStreamInit {
+			after: 10,
+			filter: FilterBuilder::default(),
+			poll_interval: Duration::from_secs(0),
+			confirmations: 10,
+		};
+
+		log_stream(transport, init).take(1)
+	},
+	expected => vec![LogStreamItem {
+		from: 0xb,
+		to: 0x1006,
+		logs: vec![Log {
+			address: "0x0000000000000000000000000000000000000001".parse().unwrap(),
+			topics: vec![],
+			data: vec![0x10].into(),
+			log_type: "".into(),
+			..Default::default()
+		}],
+	}],
+	"eth_blockNumber" =>
+		req => r#"[]"#,
+		res => r#""0x1010""#;
+	"eth_getLogs" =>
+		req => r#"[{"address":null,"fromBlock":"0xb","limit":null,"toBlock":"0x1006","topics":null}]"#,
+		res =>
+			r#"[{"address":"0x0000000000000000000000000000000000000001","topics":[],"data":"0x10","type":""}]"#;
+}
+
+test_transport_stream! {
+	name => log_stream_get_multiple_logs,
+	init => |transport| {
+		let init = LogStreamInit {
+			after: 10,
+			filter: FilterBuilder::default(),
+			poll_interval: Duration::from_secs(0),
+			confirmations: 10,
+		};
+
+		log_stream(transport, init).take(3)
+	},
+	expected => vec![LogStreamItem {
+		from: 0xb,
+		to: 0x1006,
+		logs: vec![Log {
+			address: "0x0000000000000000000000000000000000000001".parse().unwrap(),
+			topics: vec![],
+			data: vec![0x10].into(),
+			log_type: "".into(),
+			..Default::default()
+		}],
+	}, LogStreamItem {
+		from: 0x1007,
+		to: 0x1007,
+		logs: vec![],
+	}, LogStreamItem {
+		from: 0x1008,
+		to: 0x1008,
+		logs: vec![Log {
+			address: "0x0000000000000000000000000000000000000002".parse().unwrap(),
+			topics: vec![],
+			data: vec![0x20].into(),
+			log_type: "".into(),
+			..Default::default()
+		}, Log {
+			address: "0x0000000000000000000000000000000000000002".parse().unwrap(),
+			topics: vec![],
+			data: vec![0x30].into(),
+			log_type: "".into(),
+			..Default::default()
+		}],
+	}],
+	"eth_blockNumber" =>
+		req => r#"[]"#,
+		res => r#""0x1010""#;
+	"eth_getLogs" =>
+		req => r#"[{"address":null,"fromBlock":"0xb","limit":null,"toBlock":"0x1006","topics":null}]"#,
+		res => r#"[{"address":"0x0000000000000000000000000000000000000001","topics":[],"data":"0x10","type":""}]"#;
+	"eth_blockNumber" =>
+		req => r#"[]"#,
+		res => r#""0x1011""#;
+	"eth_getLogs" =>
+		req => r#"[{"address":null,"fromBlock":"0x1007","limit":null,"toBlock":"0x1007","topics":null}]"#,
+		res => r#"[]"#;
+	"eth_blockNumber" =>
+		req => r#"[]"#,
+		res => r#""0x1012""#;
+	"eth_getLogs" =>
+		req => r#"[{"address":null,"fromBlock":"0x1008","limit":null,"toBlock":"0x1008","topics":null}]"#,
+		res => r#"[{"address":"0x0000000000000000000000000000000000000002","topics":[],"data":"0x20","type":""},{"address":"0x0000000000000000000000000000000000000002","topics":[],"data":"0x30","type":""}]"#;
 }
