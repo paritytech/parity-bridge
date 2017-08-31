9d9feb756e2a5cfe222af355080fd7650dbf8559
debris <marek.kotewicz@gmail.com>
2017-08-31 14:57:43 +0200

    tuned contract deploy

--- tools/solc_compile.sh
+++ tools/solc_compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd contracts
-solc --abi --bin -o . --overwrite bridge.sol
+solc --optimize --abi --bin -o . --overwrite bridge.sol
 
 for abi in *.abi; do
 	python -m json.tool "$abi" > tmp
