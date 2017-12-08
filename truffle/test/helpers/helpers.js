c6c35c3764950ffdc408e1a92200258cb241c668
Maximilian Krüger <kruemaxi@googlemail.com>
2017-12-08 12:23:21 +0100

    add test/helpers.padLeft

--- truffle/test/helpers/helpers.js
+++ truffle/test/helpers/helpers.js
@@ -73,3 +73,14 @@ function bigNumberToPaddedBytes32(num) {
   return "0x" + result;
 }
 module.exports.bigNumberToPaddedBytes32 = bigNumberToPaddedBytes32;
+
+// repeatedly add `padding` to the left of `input` while `input`
+// is smaller than `requestedLength`
+function padLeft(input, padding, requestedLength) {
+    var output = input;
+    while (output.length < requestedLength) {
+        output = padding + output;
+    }
+    return output;
+}
+module.exports.padLeft = padLeft;
