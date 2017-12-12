46108267a14be6160cd61ec43d15ee91b2231712
Maximilian Krüger <kruemaxi@googlemail.com>
2017-12-12 12:54:54 +0100

    add test ""should allow second withdraw with different transactionHash but same recipient and value"

--- truffle/test/home.js
+++ truffle/test/home.js
@@ -123,6 +123,60 @@ contract('HomeBridge', function(accounts) {
     })
   })
 
+  it("should allow second withdraw with different transactionHash but same recipient and value", function() {
+    var homeBridge;
+    var message1;
+    var message2;
+    var requiredSignatures = 1;
+    var authorities = [accounts[0], accounts[1]];
+    var user_account = accounts[2];
+    var recipient_account = accounts[3];
+    var value = web3.toBigNumber(web3.toWei(1, "ether"));
+
+    return HomeBridge.new(requiredSignatures, authorities).then(function(instance) {
+      homeBridge = instance;
+      // "charge" HomeBridge so we can withdraw later
+      return homeBridge.sendTransaction({
+        value: value.times(2),
+        from: user_account
+      })
+    }).then(function(result) {
+      message1 = createMessage(recipient_account, value, "0x1045bfe274b88120a6b1e5d01b5ec00ab5d01098346e90e7c7a3c9b8f0181c80");
+      return helpers.sign(authorities[0], message1);
+    }).then(function(signature) {
+      var vrs = helpers.signatureToVRS(signature);
+      return homeBridge.withdraw(
+        [vrs.v],
+        [vrs.r],
+        [vrs.s],
+        message1,
+        {from: authorities[0]}
+      );
+    }).then(function(result) {
+      assert.equal(1, result.logs.length, "Exactly one event should be created");
+      assert.equal("Withdraw", result.logs[0].event, "Event name should be Withdraw");
+      assert.equal(recipient_account, result.logs[0].args.recipient, "Event recipient should match recipient in message");
+      assert(value.equals(result.logs[0].args.value), "Event value should match value in message");
+
+      message2 = createMessage(recipient_account, value, "0x038c79eb958a13aa71996bac27c628f33f227288bd27d5e157b97e55e08fd2b3");
+      return helpers.sign(authorities[0], message2);
+    }).then(function(signature) {
+      var vrs = helpers.signatureToVRS(signature);
+      return homeBridge.withdraw(
+        [vrs.v],
+        [vrs.r],
+        [vrs.s],
+        message2,
+        {from: authorities[0]}
+      );
+    }).then(function(result) {
+      assert.equal(1, result.logs.length, "Exactly one event should be created");
+      assert.equal("Withdraw", result.logs[0].event, "Event name should be Withdraw");
+      assert.equal(recipient_account, result.logs[0].args.recipient, "Event recipient should match recipient in message");
+      assert(value.equals(result.logs[0].args.value), "Event value should match value in message");
+    })
+  })
+
   it("withdraw without funds on HomeBridge should fail", function() {
     var homeBridge;
     var signature;
