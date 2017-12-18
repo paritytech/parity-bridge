f637eca51978c7704287066005ab8e1c31e1e10c
Maximilian Krüger <kruemaxi@googlemail.com>
2017-12-18 12:08:46 +0100

    sol-rs: bridge_tests.rs: fix tabs, cleanup test, assert events

--- solaris-testing/tests/bridge_tests.rs
+++ solaris-testing/tests/bridge_tests.rs
@@ -17,70 +17,60 @@ use types::{U256, H256, Address};
 use_contract!(foreign_bridge, "ForeignBridge", "contracts/bridge_sol_ForeignBridge.abi");
 
 fn log_entry_to_raw_log(log_entry: &ethcore::log_entry::LogEntry) -> ethabi::RawLog {
-    let topics: Vec<ethabi::Hash> = log_entry.topics.iter().map(|x| x.0).collect();
-    ethabi::RawLog::from((topics, log_entry.data.clone()))
+	let topics: Vec<ethabi::Hash> = log_entry.topics.iter().map(|x| x.0).collect();
+	ethabi::RawLog::from((topics, log_entry.data.clone()))
 }
 
 #[test]
 fn should_allow_a_single_authority_to_confirm_a_deposit() {
-    let contract = foreign_bridge::ForeignBridge::default();
-    let code_hex = include_str!("../contracts/bridge_sol_ForeignBridge.bin");
-    let code_bytes = code_hex.from_hex().unwrap();
+	let contract = foreign_bridge::ForeignBridge::default();
+	let code_hex = include_str!("../contracts/bridge_sol_ForeignBridge.bin");
+	let code_bytes = code_hex.from_hex().unwrap();
 
-    let mut evm = solaris::evm();
+	let mut evm = solaris::evm();
 
-    let authority_addresses = vec![
-        sol::address(10),
-        sol::address(11),
-    ];
+	let authority_addresses = vec![
+		sol::address(10),
+		sol::address(11),
+	];
 
-    let required_signatures: U256 = 1.into();
+	let required_signatures: U256 = 1.into();
 
-    let contract_owner_address: Address = 3.into();
-    let user_address: Address = 1.into();
+	let contract_owner_address: Address = 3.into();
+	let user_address: Address = 1.into();
 
-    let constructor_result = contract.constructor(
-        code_bytes,
-        required_signatures,
-        authority_addresses.iter().cloned()
-    );
+	let constructor_result = contract.constructor(
+		code_bytes,
+		required_signatures,
+		authority_addresses.iter().cloned()
+	);
 
-    let transaction_hash: H256 = "0xe55bb43c36cdf79e23b4adc149cdded921f0d482e613c50c6540977c213bc408".into();
-    // TODO ether to wei
-    let value: U256 = 1.into();
+	let transaction_hash: H256 = "0xe55bb43c36cdf79e23b4adc149cdded921f0d482e613c50c6540977c213bc408".into();
+	// TODO ether to wei
+	let value: U256 = 1.into();
 
-    let contract_address = evm
-        .with_sender(contract_owner_address)
-        .with_gas(4_000_000.into())
-        .deploy(&constructor_result)
-        .unwrap();
+	let contract_address = evm
+		.with_sender(contract_owner_address)
+		.with_gas(4_000_000.into())
+		.deploy(&constructor_result)
+		.unwrap();
 
-    let fns = contract.functions();
+	let fns = contract.functions();
 
-    let result = evm
-        .with_sender(authority_addresses[0].clone())
-        .with_gas(4_000_000.into())
-        .transact(fns.deposit().input(user_address, value, transaction_hash));
+	let result = evm
+		.with_sender(authority_addresses[0].clone())
+		.with_gas(4_000_000.into())
+		.transact(fns.deposit().input(user_address, value, transaction_hash));
 
-    assert_eq!(
-        evm.logs(filter).len(),
-        1,
-        "exactly one deposit event should be created");
+	assert_eq!(
+		evm.logs().len(),
+		1,
+		"exactly one event should be created");
 
-	let filter = foreign_bridge::events::Deposit::default().create_filter();
-    for log in evm.logs(filter) {
-        println!("log entry = {:?}", log);
-    }
-
-    // assert_eq!(
-    //     U256::from_big_endian(&*evm.call(fns.is_authority().input(user_address)).unwrap()),
-    //     sol::uint(0)
-    // );
-    //
-    // for authority_address in authority_addresses.iter() {
-    //     assert_eq!(
-    //         U256::from_big_endian(&*evm.call(fns.is_authority().input(*authority_address)).unwrap()),
-    //         sol::uint(1)
-    //     );
-    // }
+	let log_entry = &evm.logs()[0];
+	let raw_log = log_entry_to_raw_log(log_entry);
+	let deposit_log = contract.events().deposit().parse_log(raw_log)
+		.expect("the event should be a deposit event");
+	assert_eq!(Address::from(deposit_log.recipient), user_address);
+	assert_eq!(U256::from(deposit_log.value), value);
 }
