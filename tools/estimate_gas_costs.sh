ee49dc5c3b25b6b9192d766de23a26ea51e40fd1
Maximilian Krüger <kruemaxi@googlemail.com>
2018-01-04 16:27:49 +0100

    tools/estimate_gas_costs.sh: improve wording

--- tools/estimate_gas_costs.sh
+++ tools/estimate_gas_costs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # prints out estimated gas costs of contract functions.
-# runs the tests which estimate and print out gas costs and greps by gas costs.
+# runs the tests which estimate and print out gas costs. then greps test output for gas costs.
 
 cd truffle
 yarn test | grep "estimated gas cost"
