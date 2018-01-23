810f01314045b7ad7136592561b80de1d16afec2
Maximilian Krüger <kruemaxi@googlemail.com>
2018-01-23 12:49:01 +0100

    foreign-erc20.js: more test cases

--- truffle/test/foreign-erc20.js
+++ truffle/test/foreign-erc20.js
@@ -39,7 +39,7 @@ contract('ForeignBridge', function(accounts) {
     var requiredSignatures = 1;
     var authorities = [accounts[0], accounts[1]];
 	var owner = accounts[2];
-	var approved = accounts[3];
+	var spender = accounts[3];
 	var receiver = accounts[4];
     var hash = "0xe55bb43c36cdf79e23b4adc149cdded921f0d482e613c50c6540977c213bc408";
 
@@ -50,23 +50,66 @@ contract('ForeignBridge', function(accounts) {
 	  return contract.deposit(owner, web3.toWei(3, "ether"), hash, {from: authorities[0]});
 	}).then(function(result) {
 
-      return contract.allowance(owner, approved);
+      return contract.allowance(owner, spender);
     }).then(function(result) {
-	  assert.equal(0, result, "user should have 0 allowance initially");
+	  assert.equal(0, result, "initial allowance should be 0");
 
-      return contract.transferFrom(owner, receiver, web3.toWei(1, "ether"));
+      return contract.transferFrom(owner, receiver, web3.toWei(1, "ether"), {from: spender});
     }).then(function(result) {
 	  assert(false, "transfer without allowance should fail");
 
 	}, function(err) {
-	  return contract.approve(approved, web3.toWei(2, "ether"), {from: owner});
+	  return contract.approve(spender, web3.toWei(4, "ether"), {from: owner});
+	}).then(function(result) {
+      assert.equal(1, result.logs.length, "Exactly one event should be created");
+      assert.equal("Approval", result.logs[0].event, "Event name should be Approval");
+      assert.equal(owner, result.logs[0].args.tokenOwner);
+      assert.equal(spender, result.logs[0].args.spender);
+      assert.equal(web3.toWei(4, "ether"), result.logs[0].args.tokens);
+
+      return contract.allowance(owner, spender);
+    }).then(function(result) {
+	  assert.equal(web3.toWei(4, "ether"), result, "approval should set allowance");
+
+      return contract.transferFrom(owner, receiver, web3.toWei(4, "ether"), {from: spender});
+    }).then(function(result) {
+	  assert(false, "transferring more than balance should fail");
+	}, function(err) {
+	  return contract.approve(spender, web3.toWei(2, "ether"), {from: owner});
 	}).then(function(result) {
 	  console.log(result);
       assert.equal(1, result.logs.length, "Exactly one event should be created");
       assert.equal("Approval", result.logs[0].event, "Event name should be Approval");
       assert.equal(owner, result.logs[0].args.tokenOwner);
-      assert.equal(approved, result.logs[0].args.spender);
+      assert.equal(spender, result.logs[0].args.spender);
       assert.equal(web3.toWei(2, "ether"), result.logs[0].args.tokens);
+
+      return contract.allowance(owner, spender);
+    }).then(function(result) {
+	  assert.equal(web3.toWei(2, "ether"), result, "approval should update allowance");
+
+      return contract.transferFrom(owner, receiver, web3.toWei(2, "ether") + 2, {from: spender});
+    }).then(function(result) {
+	  assert(false, "transferring more than allowance should fail");
+	}, function(err) {
+      return contract.transferFrom(owner, receiver, web3.toWei(2, "ether"), {from: spender});
+    }).then(function(result) {
+
+      return contract.balanceOf(owner);
+    }).then(function(result) {
+	  assert.equal(web3.toWei(1, "ether"), result, "transferring should reduce owners balance");
+
+      return contract.balanceOf(receiver);
+    }).then(function(result) {
+	  assert.equal(web3.toWei(2, "ether"), result, "transferring should increase receivers balance");
+
+      return contract.balanceOf(spender);
+    }).then(function(result) {
+	  assert.equal(0, result, "transferring should not modify spenders balance");
+
+      return contract.allowance(owner, spender);
+    }).then(function(result) {
+	  assert.equal(0, result, "transferring whole allowance should set allowance to 0");
 	})
   })
 })
