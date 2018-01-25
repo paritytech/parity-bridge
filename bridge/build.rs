8a8380e7a88023fd50b03c8895636710d5bde134
Maximilian Krüger <kruemaxi@googlemail.com>
2018-01-25 09:51:22 +0100

    bridge/build.rs: make error messages helpful. resolves #98

--- bridge/build.rs
+++ bridge/build.rs
@@ -1,18 +1,35 @@
 use std::process::Command;
 
 fn main() {
-    // rerun build script if bridge contract has changed.
-    // without this cargo doesn't since the bridge contract
-    // is outside the crate directories
-    println!("cargo:rerun-if-changed=../contracts/bridge.sol");
-    let exit_status = Command::new("solc")
-        .arg("--abi")
-        .arg("--bin")
-        .arg("--optimize")
-        .arg("--output-dir").arg("../compiled_contracts")
-        .arg("--overwrite")
-        .arg("../contracts/bridge.sol")
-        .status()
-		.unwrap_or_else(|e| panic!("Error compiling solidity contracts: {}", e));
-	assert!(exit_status.success(), "There was an error while compiling contracts code.");
+	// rerun build script if bridge contract has changed.
+	// without this cargo doesn't since the bridge contract
+	// is outside the crate directories
+	println!("cargo:rerun-if-changed=../contracts/bridge.sol");
+
+	match Command::new("solc")
+		.arg("--abi")
+		.arg("--bin")
+		.arg("--optimize")
+		.arg("--output-dir").arg("../compiled_contracts")
+		.arg("--overwrite")
+		.arg("../contracts/bridge.sol")
+		.status()
+	{
+		Ok(exit_status) => {
+			if !exit_status.success() {
+				if let Some(code) = exit_status.code() {
+					panic!("`solc` exited with error exit status code `{}`", code);
+				} else {
+					panic!("`solc` exited because it was terminated by a signal");
+				}
+			}
+		},
+		Err(err) => {
+			if let std::io::ErrorKind::NotFound = err.kind() {
+				panic!("`solc` executable not found in `$PATH`. `solc` is required to compile the bridge contracts. please install it: https://solidity.readthedocs.io/en/develop/installing-solidity.html");
+			} else {
+				panic!("an error occurred when trying to spawn `solc`: {}", err);
+			}
+		}
+	}
 }
