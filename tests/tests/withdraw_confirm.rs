3cc6b294385c4c8a4abc0f66efc12acc2fc9f497
Maximilian Krüger <kruemaxi@googlemail.com>
2018-02-12 16:15:19 +0100

    tests/tests/withdraw_confirm.rs: refactor: replace opaque data strings by something easily modifiable (json, ethabi use)

--- tests/tests/withdraw_confirm.rs
+++ tests/tests/withdraw_confirm.rs
@@ -1,9 +1,22 @@
+/// test interactions of withdraw_confirm state machine with RPC
+
 extern crate futures;
+#[macro_use]
+extern crate serde_json;
 extern crate bridge;
 #[macro_use]
 extern crate tests;
+extern crate ethabi;
+extern crate rustc_hex;
+extern crate ethereum_types;
 
+use rustc_hex::{ToHex, FromHex};
 use bridge::bridge::create_withdraw_confirm;
+use bridge::message_to_mainnet::MessageToMainnet;
+use bridge::contracts;
+use ethabi::{encode, Token};
+
+const WITHDRAW_TOPIC: &str = "0xf279e6a1f5e320cca91135676d9cb6e44ca8a08c0b88342bcdb1144f6511b568";
 
 test_app_stream! {
 	name => withdraw_confirm_basic,
@@ -26,17 +39,29 @@ test_app_stream! {
 	home_transport => [],
 	foreign_transport => [
 		"eth_blockNumber" =>
-			req => r#"[]"#,
-			res => r#""0x1011""#;
+			req => json!([]),
+			res => json!("0x1011");
 		"eth_getLogs" =>
-			req => r#"[{"address":["0x0000000000000000000000000000000000000000"],"fromBlock":"0x1","limit":null,"toBlock":"0x1005","topics":[["0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"],null,null,null]}]"#,
-			res => r#"[]"#;
+			req => json!([{
+				"address": ["0x0000000000000000000000000000000000000000"],
+				"fromBlock": "0x1",
+				"limit": null,
+				"toBlock": "0x1005",
+				"topics": [[WITHDRAW_TOPIC], null, null, null]
+			}]),
+			res => json!([]);
 		"eth_blockNumber" =>
-			req => r#"[]"#,
-			res => r#""0x1012""#;
+			req => json!([]),
+			res => json!("0x1012");
 		"eth_getLogs" =>
-			req => r#"[{"address":["0x0000000000000000000000000000000000000000"],"fromBlock":"0x1006","limit":null,"toBlock":"0x1006","topics":[["0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"],null,null,null]}]"#,
-			res => r#"[]"#;
+			req => json!([{
+				"address": ["0x0000000000000000000000000000000000000000"],
+				"fromBlock": "0x1006",
+				"limit": null,
+				"toBlock": "0x1006",
+				"topics":[[WITHDRAW_TOPIC], null, null, null]
+			}]),
+			res => json!([]);
 	]
 }
 
@@ -64,20 +89,31 @@ test_app_stream! {
 	home_transport => [],
 	foreign_transport => [
 		"eth_blockNumber" =>
-			req => r#"[]"#,
-			res => r#""0x0100""#;
+			req => json!([]),
+			res => json!("0x0100");
 		"eth_blockNumber" =>
-			req => r#"[]"#,
-			res => r#""0x1011""#;
+			req => json!([]),
+			res => json!("0x1011");
 		"eth_getLogs" =>
-			req => r#"[{"address":["0x0000000000000000000000000000000000000000"],"fromBlock":"0xf6","limit":null,"toBlock":"0x1005","topics":[["0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"],null,null,null]}]"#,
-			res => r#"[]"#;
+			req => json!([{
+				"address":["0x0000000000000000000000000000000000000000"],
+				"fromBlock": "0xf6",
+				"limit": null, "toBlock": "0x1005",
+				"topics": [[WITHDRAW_TOPIC], null, null, null]
+			}]),
+			res => json!([]);
 		"eth_blockNumber" =>
-			req => r#"[]"#,
-			res => r#""0x1012""#;
+			req => json!([]),
+			res => json!("0x1012");
 		"eth_getLogs" =>
-			req => r#"[{"address":["0x0000000000000000000000000000000000000000"],"fromBlock":"0x1006","limit":null,"toBlock":"0x1006","topics":[["0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"],null,null,null]}]"#,
-			res => r#"[]"#;
+			req => json!([{
+				"address":["0x0000000000000000000000000000000000000000"],
+				"fromBlock": "0x1006",
+				"limit": null,
+				"toBlock": "0x1006",
+				"topics":[[WITHDRAW_TOPIC], null, null, null]
+			}]),
+			res => json!([]);
 	]
 }
 
@@ -107,20 +143,32 @@ test_app_stream! {
 	home_transport => [],
 	foreign_transport => [
 		"eth_blockNumber" =>
-			req => r#"[]"#,
-			res => r#""0x0100""#;
+			req => json!([]),
+			res => json!("0x0100");
 		"eth_blockNumber" =>
-			req => r#"[]"#,
-			res => r#""0x1011""#;
+			req => json!([]),
+			res => json!("0x1011");
 		"eth_getLogs" =>
-			req => r#"[{"address":["0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"],"fromBlock":"0xf6","limit":null,"toBlock":"0x1005","topics":[["0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"],null,null,null]}]"#,
-			res => r#"[]"#;
+			req => json!([{
+				"address":["0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"],
+				"fromBlock": "0xf6",
+				"limit": null,
+				"toBlock": "0x1005",
+				"topics":[[WITHDRAW_TOPIC],null,null,null]
+			}]),
+			res => json!([]);
 		"eth_blockNumber" =>
-			req => r#"[]"#,
-			res => r#""0x1012""#;
+			req => json!([]),
+			res => json!("0x1012");
 		"eth_getLogs" =>
-			req => r#"[{"address":["0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"],"fromBlock":"0x1006","limit":null,"toBlock":"0x1006","topics":[["0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"],null,null,null]}]"#,
-			res => r#"[]"#;
+			req => json!([{
+				"address": ["0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"],
+				"fromBlock": "0x1006",
+				"limit": null,
+				"toBlock": "0x1006",
+				"topics":[[WITHDRAW_TOPIC],null,null,null]
+			}]),
+			res => json!([]);
 	]
 }
 
@@ -156,26 +204,77 @@ test_app_stream! {
 	home_transport => [],
 	foreign_transport => [
 		"eth_blockNumber" =>
-			req => r#"[]"#,
-			res => r#""0x0100""#;
+			req => json!([]),
+			res => json!("0x0100");
 		"eth_blockNumber" =>
-			req => r#"[]"#,
-			res => r#""0x1011""#;
+			req => json!([]),
+			res => json!("0x1011");
 		"eth_getLogs" =>
-			req => r#"[{"address":["0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"],"fromBlock":"0xf6","limit":null,"toBlock":"0x1005","topics":[["0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"],null,null,null]}]"#,
-			res => r#"[{"address":"0x49edf201c1e139282643d5e7c6fb0c7219ad1db8","topics":["0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"],"data":"0x000000000000000000000000aff3454fce5edbc8cca8697c15331677e6ebcccc00000000000000000000000000000000000000000000000000000000000000f0","type":"","transactionHash":"0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"}]"#;
+			req => json!([{
+				"address": ["0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"],
+				"fromBlock": "0xf6",
+				"limit": null,
+				"toBlock": "0x1005",
+				"topics": [[WITHDRAW_TOPIC], null, null, null]
+			}]),
+			res => json!([{
+				"address": "0x49edf201c1e139282643d5e7c6fb0c7219ad1db8",
+				"topics": [WITHDRAW_TOPIC],
+				"data": format!("0x{}", encode(&[
+					Token::Address([1u8; 20].into()),
+					Token::Uint(10000.into()),
+					Token::Uint(1000.into()),
+				]).to_hex()),
+				"type": "",
+				"transactionHash": "0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"
+			}]);
 		"eth_sign" =>
-			req => r#"["0x0000000000000000000000000000000000000001","0xaff3454fce5edbc8cca8697c15331677e6ebcccc00000000000000000000000000000000000000000000000000000000000000f0884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"]"#,
-			res => r#""0x8697c15331677e6ebccccaff3454fce5edbc8cca8697c15331677aff3454fce5edbc8cca8697c15331677e6ebccccaff3454fce5edbc8cca8697c15331677e6ebc""#;
+			req => json!([
+				"0x0000000000000000000000000000000000000001",
+				format!("0x{}", MessageToMainnet {
+					recipient: [1u8; 20].into(),
+					value: 10000.into(),
+					sidenet_transaction_hash: "0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364".into(),
+					mainnet_gas_price: 1000.into(),
+				}
+				.to_bytes()
+				.to_hex())
+			]),
+			res => json!("0x8697c15331677e6ebccccaff3454fce5edbc8cca8697c15331677aff3454fce5edbc8cca8697c15331677e6ebccccaff3454fce5edbc8cca8697c15331677e6ebc");
+		// `submitSignature`
 		"eth_sendTransaction" =>
-			req => r#"[{"data":"0x630cea8e000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000c000000000000000000000000000000000000000000000000000000000000000418697c15331677e6ebccccaff3454fce5edbc8cca8697c15331677aff3454fce5edbc8cca8697c15331677e6ebccccaff3454fce5edbc8cca8697c15331677e6ebc000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000054aff3454fce5edbc8cca8697c15331677e6ebcccc00000000000000000000000000000000000000000000000000000000000000f0884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364000000000000000000000000","from":"0x0000000000000000000000000000000000000001","gas":"0xfe","gasPrice":"0xa1","to":"0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"}]"#,
-			res => r#""0x1db8f385535c0d178b8f40016048f3a3cffee8f94e68978ea4b277f57b638f0b""#;
+			req => json!([{
+				"data": format!("0x{}", contracts::foreign::ForeignBridge::default()
+					.functions()
+					.submit_signature()
+					.input(
+						"8697c15331677e6ebccccaff3454fce5edbc8cca8697c15331677aff3454fce5edbc8cca8697c15331677e6ebccccaff3454fce5edbc8cca8697c15331677e6ebc".from_hex().unwrap(),
+						MessageToMainnet {
+							recipient: [1u8; 20].into(),
+							value: 10000.into(),
+							sidenet_transaction_hash: "0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364".into(),
+							mainnet_gas_price: 1000.into(),
+						}.to_bytes()
+					)
+					.to_hex()),
+				"from": "0x0000000000000000000000000000000000000001",
+				"gas": "0xfe",
+				"gasPrice": "0xa1",
+				"to":"0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"
+			}]),
+			res => json!("0x1db8f385535c0d178b8f40016048f3a3cffee8f94e68978ea4b277f57b638f0b");
 		"eth_blockNumber" =>
-			req => r#"[]"#,
-			res => r#""0x1012""#;
+			req => json!([]),
+			res => json!("0x1012");
 		"eth_getLogs" =>
-			req => r#"[{"address":["0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"],"fromBlock":"0x1006","limit":null,"toBlock":"0x1006","topics":[["0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"],null,null,null]}]"#,
-			res => r#"[]"#;
+			req => json!([{
+				"address": ["0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"],
+				"fromBlock": "0x1006",
+				"limit": null,
+				"toBlock": "0x1006",
+				"topics":[[WITHDRAW_TOPIC], null, null, null]
+			}]),
+			res => json!([]);
 	]
 }
 
@@ -210,28 +309,118 @@ test_app_stream! {
 	home_transport => [],
 	foreign_transport => [
 		"eth_blockNumber" =>
-			req => r#"[]"#,
-			res => r#""0xe""#;
+			req => json!([]),
+			res => json!("0xe");
 		"eth_getLogs" =>
-			req => r#"[{"address":["0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"],"fromBlock":"0x1","limit":null,"toBlock":"0x2","topics":[["0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"],null,null,null]}]"#,
-			res => r#"[{"address":"0x49edf201c1e139282643d5e7c6fb0c7219ad1db8","topics":["0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"],"data":"0x000000000000000000000000aff3454fce5edbc8cca8697c15331677e6ebcccc00000000000000000000000000000000000000000000000000000000000000f0","type":"","transactionHash":"0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"},{"address":"0x49edf201c1e139282643d5e7c6fb0c7219ad1db8","topics":["0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"],"data":"0x000000000000000000000000001da5bcab735024168f00b43abcc9ef522392e90000000000000000000000000000000000000000000000000000000000000099","type":"","transactionHash":"0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424399"}]"#;
+			req => json!([{
+				"address": ["0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"],
+				"fromBlock": "0x1",
+				"limit": null,
+				"toBlock": "0x2",
+				"topics": [[WITHDRAW_TOPIC], null, null, null]
+			}]),
+			res => json!([{
+				"address": "0x49edf201c1e139282643d5e7c6fb0c7219ad1db8",
+				"topics": [WITHDRAW_TOPIC],
+				"data": format!("0x{}", encode(&[
+					Token::Address([1u8; 20].into()),
+					Token::Uint(10000.into()),
+					Token::Uint(1000.into()),
+				]).to_hex()),
+				"type": "",
+				"transactionHash": "0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"},
+			{
+				"address":"0x49edf201c1e139282643d5e7c6fb0c7219ad1db8",
+				"topics": [WITHDRAW_TOPIC],
+				"data": format!("0x{}", encode(&[
+					Token::Address([2u8; 20].into()),
+					Token::Uint(42.into()),
+					Token::Uint(100.into()),
+				]).to_hex()),
+				"type":"",
+				"transactionHash":"0xfffedad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"
+			}]);
 		"eth_sign" =>
-			req => r#"["0x0000000000000000000000000000000000000001","0xaff3454fce5edbc8cca8697c15331677e6ebcccc00000000000000000000000000000000000000000000000000000000000000f0884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"]"#,
-			res => r#""0x8697c15331677e6ebccccaff3454fce5edbc8cca8697c15331677aff3454fce5edbc8cca8697c15331677e6ebccccaff3454fce5edbc8cca8697c15331677e6ebc""#;
+			req => json!([
+				"0x0000000000000000000000000000000000000001",
+				format!("0x{}", MessageToMainnet {
+					recipient: [1u8; 20].into(),
+					value: 10000.into(),
+					sidenet_transaction_hash: "0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364".into(),
+					mainnet_gas_price: 1000.into(),
+				}
+				.to_bytes()
+				.to_hex())
+			]),
+			res => json!("0x8697c15331677e6ebccccaff3454fce5edbc8cca8697c15331677aff3454fce5edbc8cca8697c15331677e6ebccccaff3454fce5edbc8cca8697c15331677e6ebc");
 		"eth_sign" =>
-			req => r#"["0x0000000000000000000000000000000000000001","0x001da5bcab735024168f00b43abcc9ef522392e90000000000000000000000000000000000000000000000000000000000000099884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424399"]"#,
-			res => r#""0x8a3b24c56e46f6fc9fa7ed14795745348059b8ac84d6ee93323e83a429e760ae6e89510834ee4d65eefacd74cddca53df61b5eba1c3007ed88d2eebff2e0e2151b""#;
+			req => json!([
+				"0x0000000000000000000000000000000000000001",
+				format!("0x{}", MessageToMainnet {
+					recipient: [2u8; 20].into(),
+					value: 42.into(),
+					sidenet_transaction_hash: "0xfffedad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364".into(),
+					mainnet_gas_price: 100.into(),
+				}
+				.to_bytes()
+				.to_hex())
+			]),
+			res => json!("0x8a3b24c56e46f6fc9fa7ed14795745348059b8ac84d6ee93323e83a429e760ae6e89510834ee4d65eefacd74cddca53df61b5eba1c3007ed88d2eebff2e0e2151b");
+		// `submitSignature`
 		"eth_sendTransaction" =>
-			req => r#"[{"data":"0x630cea8e000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000c000000000000000000000000000000000000000000000000000000000000000418697c15331677e6ebccccaff3454fce5edbc8cca8697c15331677aff3454fce5edbc8cca8697c15331677e6ebccccaff3454fce5edbc8cca8697c15331677e6ebc000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000054aff3454fce5edbc8cca8697c15331677e6ebcccc00000000000000000000000000000000000000000000000000000000000000f0884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364000000000000000000000000","from":"0x0000000000000000000000000000000000000001","gas":"0xff","gasPrice":"0xaa","to":"0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"}]"#,
-			res => r#""0x1db8f385535c0d178b8f40016048f3a3cffee8f94e68978ea4b277f57b638f0b""#;
+			req => json!([{
+				"data": format!("0x{}", contracts::foreign::ForeignBridge::default()
+					.functions()
+					.submit_signature()
+					.input(
+						"8697c15331677e6ebccccaff3454fce5edbc8cca8697c15331677aff3454fce5edbc8cca8697c15331677e6ebccccaff3454fce5edbc8cca8697c15331677e6ebc".from_hex().unwrap(),
+						MessageToMainnet {
+							recipient: [1u8; 20].into(),
+							value: 10000.into(),
+							sidenet_transaction_hash: "0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364".into(),
+							mainnet_gas_price: 1000.into(),
+						}.to_bytes()
+					)
+					.to_hex()),
+				"from": "0x0000000000000000000000000000000000000001",
+				"gas": "0xff",
+				"gasPrice": "0xaa",
+				"to":"0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"
+			}]),
+			res => json!("0x1db8f385535c0d178b8f40016048f3a3cffee8f94e68978ea4b277f57b638f0b");
+		// `submitSignature`
 		"eth_sendTransaction" =>
-			req => r#"[{"data":"0x630cea8e000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000c000000000000000000000000000000000000000000000000000000000000000418a3b24c56e46f6fc9fa7ed14795745348059b8ac84d6ee93323e83a429e760ae6e89510834ee4d65eefacd74cddca53df61b5eba1c3007ed88d2eebff2e0e2151b000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000054001da5bcab735024168f00b43abcc9ef522392e90000000000000000000000000000000000000000000000000000000000000099884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424399000000000000000000000000","from":"0x0000000000000000000000000000000000000001","gas":"0xff","gasPrice":"0xaa","to":"0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"}]"#,
-			res => r#""0x1db8f385535c0d178b8f40016048f3a3cffee8f94e68978ea4b277f57b638f0c""#;
+			req => json!([{
+				"data": format!("0x{}", contracts::foreign::ForeignBridge::default()
+					.functions()
+					.submit_signature()
+					.input(
+						"8a3b24c56e46f6fc9fa7ed14795745348059b8ac84d6ee93323e83a429e760ae6e89510834ee4d65eefacd74cddca53df61b5eba1c3007ed88d2eebff2e0e2151b".from_hex().unwrap(),
+						MessageToMainnet {
+							recipient: [2u8; 20].into(),
+							value: 42.into(),
+							sidenet_transaction_hash: "0xfffedad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364".into(),
+							mainnet_gas_price: 100.into(),
+						}.to_bytes()
+					)
+					.to_hex()),
+				"from": "0x0000000000000000000000000000000000000001",
+				"gas": "0xff",
+				"gasPrice": "0xaa",
+				"to":"0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"
+			}]),
+			res => json!("0x1db8f385535c0d178b8f40016048f3a3cffee8f94e68978ea4b277f57b638f0c");
 		"eth_blockNumber" =>
-			req => r#"[]"#,
-			res => r#""0x1012""#;
+			req => json!([]),
+			res => json!("0x1012");
 		"eth_getLogs" =>
-			req => r#"[{"address":["0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"],"fromBlock":"0x3","limit":null,"toBlock":"0x1006","topics":[["0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364"],null,null,null]}]"#,
-			res => r#"[]"#;
+			req => json!([{
+				"address": ["0x49edf201c1e139282643d5e7c6fb0c7219ad1db8"],
+				"fromBlock": "0x3",
+				"limit": null,
+				"toBlock": "0x1006",
+				"topics":[[WITHDRAW_TOPIC], null, null, null]
+			}]),
+			res => json!([]);
 	]
 }
