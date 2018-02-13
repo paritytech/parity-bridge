2165513fdd43521266803541bbf7bc14299dc683
Maximilian Krüger <kruemaxi@googlemail.com>
2018-02-13 14:26:11 +0100

    tests for Helpers.verifySignatures

--- truffle/test/helpers.js
+++ truffle/test/helpers.js
@@ -80,4 +80,201 @@ contract("Helpers", function(accounts) {
       assert.equal(result, "131242344353464564564574574567456");
     })
   })
+
+  it("`verifySignatures` should pass for 1 required signature", function() {
+    var library;
+    var signature;
+    var requiredSignatures = 1;
+    var authorities = [accounts[0], accounts[1]];
+    var recipientAccount = accounts[2];
+    var value = web3.toBigNumber(web3.toWei(1, "ether"));
+    var homeGasPrice = web3.toBigNumber(10000);
+    var message = helpers.createMessage(recipientAccount, value, "0x1045bfe274b88120a6b1e5d01b5ec00ab5d01098346e90e7c7a3c9b8f0181c80", homeGasPrice);
+
+    return Helpers.new().then(function(instance) {
+      library = instance;
+    }).then(function(result) {
+      return helpers.sign(authorities[0], message);
+    }).then(function(result) {
+      signature = result;
+      var vrs = helpers.signatureToVRS(signature);
+
+	  return library.verifySignatures.call(
+		message,
+        [vrs.v],
+        [vrs.r],
+        [vrs.s],
+		authorities,
+		requiredSignatures
+	  )
+	})
+  })
+
+  it("`verifySignatures` should pass for multiple signatures", function() {
+    var library;
+    var signatures = [];
+    var requiredSignatures = 3;
+    var authorities = [accounts[0], accounts[1], accounts[2]];
+    var recipientAccount = accounts[3];
+    var value = web3.toBigNumber(web3.toWei(1, "ether"));
+    var homeGasPrice = web3.toBigNumber(10000);
+    var message = helpers.createMessage(recipientAccount, value, "0x1045bfe274b88120a6b1e5d01b5ec00ab5d01098346e90e7c7a3c9b8f0181c80", homeGasPrice);
+
+    return Helpers.new().then(function(instance) {
+      library = instance;
+    }).then(function(result) {
+
+      return helpers.sign(authorities[0], message);
+    }).then(function(result) {
+      signatures[0] = result;
+
+      return helpers.sign(authorities[1], message);
+    }).then(function(result) {
+      signatures[1] = result;
+
+      return helpers.sign(authorities[2], message);
+    }).then(function(result) {
+      signatures[2] = result;
+
+	  var vrs = [];
+      vrs[0] = helpers.signatureToVRS(signatures[0]);
+      vrs[1] = helpers.signatureToVRS(signatures[1]);
+      vrs[2] = helpers.signatureToVRS(signatures[2]);
+
+	  return library.verifySignatures.call(
+		message,
+        [vrs[0].v, vrs[1].v, vrs[2].v],
+        [vrs[0].r, vrs[1].r, vrs[2].r],
+        [vrs[0].s, vrs[1].s, vrs[2].s],
+		authorities,
+		requiredSignatures
+	  )
+	})
+  })
+
+  it("`verifySignatures` should fail for signature for other message", function() {
+    var library;
+    var signature;
+    var requiredSignatures = 1;
+    var authorities = [accounts[0], accounts[1]];
+    var recipientAccount = accounts[2];
+    var value = web3.toBigNumber(web3.toWei(1, "ether"));
+    var homeGasPrice = web3.toBigNumber(10000);
+    var homeGasPrice2 = web3.toBigNumber(100);
+    var message = helpers.createMessage(recipientAccount, value, "0x1045bfe274b88120a6b1e5d01b5ec00ab5d01098346e90e7c7a3c9b8f0181c80", homeGasPrice);
+    var message2 = helpers.createMessage(recipientAccount, value, "0x1045bfe274b88120a6b1e5d01b5ec00ab5d01098346e90e7c7a3c9b8f0181c80", homeGasPrice2);
+
+    return Helpers.new().then(function(instance) {
+      library = instance;
+    }).then(function(result) {
+      return helpers.sign(authorities[0], message);
+    }).then(function(result) {
+      signature = result;
+      var vrs = helpers.signatureToVRS(signature);
+
+	  return library.verifySignatures.call(
+		message2,
+        [vrs.v],
+        [vrs.r],
+        [vrs.s],
+		authorities,
+		requiredSignatures
+      ).then(function() {
+        assert(false, "should fail");
+      }, helpers.ignoreExpectedError)
+	})
+  })
+
+  it("`verifySignatures` should fail if signer not in addresses", function() {
+    var library;
+    var signature;
+    var requiredSignatures = 1;
+    var authorities = [accounts[0], accounts[1]];
+    var recipientAccount = accounts[2];
+    var value = web3.toBigNumber(web3.toWei(1, "ether"));
+    var homeGasPrice = web3.toBigNumber(10000);
+    var message = helpers.createMessage(recipientAccount, value, "0x1045bfe274b88120a6b1e5d01b5ec00ab5d01098346e90e7c7a3c9b8f0181c80", homeGasPrice);
+
+    return Helpers.new().then(function(instance) {
+      library = instance;
+    }).then(function(result) {
+      return helpers.sign(accounts[3], message);
+    }).then(function(result) {
+      signature = result;
+      var vrs = helpers.signatureToVRS(signature);
+
+	  return library.verifySignatures.call(
+		message,
+        [vrs.v],
+        [vrs.r],
+        [vrs.s],
+		authorities,
+		requiredSignatures
+      ).then(function() {
+        assert(false, "should fail");
+      }, helpers.ignoreExpectedError)
+	})
+  })
+
+  it("`verifySignatures` should fail for not enough signatures", function() {
+    var library;
+    var signature;
+    var requiredSignatures = 2;
+    var authorities = [accounts[0], accounts[1]];
+    var recipientAccount = accounts[2];
+    var value = web3.toBigNumber(web3.toWei(1, "ether"));
+    var homeGasPrice = web3.toBigNumber(10000);
+    var message = helpers.createMessage(recipientAccount, value, "0x1045bfe274b88120a6b1e5d01b5ec00ab5d01098346e90e7c7a3c9b8f0181c80", homeGasPrice);
+
+    return Helpers.new().then(function(instance) {
+      library = instance;
+    }).then(function(result) {
+      return helpers.sign(authorities[0], message);
+    }).then(function(result) {
+      signature = result;
+      var vrs = helpers.signatureToVRS(signature);
+
+	  return library.verifySignatures.call(
+		message,
+        [vrs.v],
+        [vrs.r],
+        [vrs.s],
+		authorities,
+		requiredSignatures
+      ).then(function() {
+        assert(false, "should fail");
+      }, helpers.ignoreExpectedError)
+	})
+  })
+
+  it("`verifySignatures` should fail for duplicated signature", function() {
+    var library;
+    var signature;
+    var requiredSignatures = 2;
+    var authorities = [accounts[0], accounts[1]];
+    var recipientAccount = accounts[2];
+    var value = web3.toBigNumber(web3.toWei(1, "ether"));
+    var homeGasPrice = web3.toBigNumber(10000);
+    var message = helpers.createMessage(recipientAccount, value, "0x1045bfe274b88120a6b1e5d01b5ec00ab5d01098346e90e7c7a3c9b8f0181c80", homeGasPrice);
+
+    return Helpers.new().then(function(instance) {
+      library = instance;
+    }).then(function(result) {
+      return helpers.sign(authorities[0], message);
+    }).then(function(result) {
+      signature = result;
+      var vrs = helpers.signatureToVRS(signature);
+
+	  return library.verifySignatures.call(
+		message,
+        [vrs.v, vrs.v],
+        [vrs.r, vrs.r],
+        [vrs.s, vrs.r],
+		authorities,
+		requiredSignatures
+      ).then(function() {
+        assert(false, "should fail");
+      }, helpers.ignoreExpectedError)
+	})
+  })
 })
