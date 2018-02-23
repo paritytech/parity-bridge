d4a297464244408a9d5d60a2850eb0e1bc183a71
Maximilian Krüger <kruemaxi@googlemail.com>
2018-02-23 16:33:16 +0100

    arbitrary_message_passing.sol: fix solium errors/warnings

--- truffle/contracts/arbitrary_message_passing.sol
+++ truffle/contracts/arbitrary_message_passing.sol
@@ -4,44 +4,49 @@ pragma solidity ^0.4.17;
 contract MainBridge {
     event Send(address sender, address receiver, bytes data);
 
-    function send(address _receiver, bytes _data) public {
+    function sendToSide(address _receiver, bytes _data) public {
         Send(msg.sender, _receiver, _data);
     }
 }
+
 
 contract SideBridge {
     event Receive(address sender, address receiver, bytes data, bool success);
 
     function receive(address sender, address receiver, bytes data) public {
+        // solium-disable-next-line security/no-low-level-calls
         var success = receiver.call(data);
         Receive(sender, receiver, data, success);
     }
 }
 
-contract MainExample {
-	address public main_bridge_address;
 
-	function MainExample(address _main_bridge_address) public {
-		main_bridge_address = _main_bridge_address;
-	}
+contract MainExample {
+    address public mainBridgeAddress;
+
+    function MainExample(address _mainBridgeAddress) public {
+        mainBridgeAddress = _mainBridgeAddress;
+    }
 
     function something(address _receiver, uint256 a, uint256 b) public {
-		// need to build `data` manually for now.
-		// there will be a handy function for it in the future:
-		// https://github.com/ethereum/solidity/issues/1707
-        var func_sig = bytes4(keccak256("times(uint256,uint256)"));
-		var data = new bytes(68);
+        // need to build `data` manually for now.
+        // there will be a handy function for it in the future:
+        // https://github.com/ethereum/solidity/issues/1707
+        var funcSig = bytes4(keccak256("times(uint256,uint256)"));
+        var data = new bytes(68);
 
-		assembly {
-			// skip first 32 bytes which encode length of `bytes data`
-			mstore(add(data, 32), func_sig)
-			mstore(add(data, 36), a)
-			mstore(add(data, 68), b)
-		}
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            // skip first 32 bytes which encode length of `bytes data`
+            mstore(add(data, 32), funcSig)
+            mstore(add(data, 36), a)
+            mstore(add(data, 68), b)
+        }
 
-        MainBridge(main_bridge_address).send(_receiver, data);
+        MainBridge(main_bridge_address).sendToSide(_receiver, data);
     }
 }
+
 
 contract SideExample {
     event Times(uint256 a, uint256 b, uint256 result);
@@ -51,9 +56,9 @@ contract SideExample {
     }
 }
 
+
 /// to give each sender a unique address
 contract AddressProxy {
-  function () public {
-
-  }
+    function () public {
+    }
 }
