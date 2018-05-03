5cfd9b6d77e833e150d3b207cfca83abe4935930
Maximilian Krüger <kruemaxi@googlemail.com>
2018-05-03 18:10:10 +0200

    message to mainnet -> message to main

--- bridge/src/message_to_main.rs
+++ bridge/src/message_to_main.rs
@@ -8,17 +8,17 @@ use error::Error;
 /// contains all the information required for the relay.
 /// validators sign off on this message.
 #[derive(PartialEq, Debug)]
-pub struct MessageToMainnet {
+pub struct MessageToMain {
     pub recipient: Address,
     pub value: U256,
-    pub sidenet_transaction_hash: H256,
-    pub mainnet_gas_price: U256,
+    pub side_tx_hash: H256,
+    pub main_gas_price: U256,
 }
 
-/// length of a `MessageToMainnet.to_bytes()` in bytes
+/// length of a `MessageToMain.to_bytes()` in bytes
 pub const MESSAGE_LENGTH: usize = 116;
 
-impl MessageToMainnet {
+impl MessageToMain {
     /// parses message from a byte slice
     pub fn from_bytes(bytes: &[u8]) -> Self {
         assert_eq!(bytes.len(), MESSAGE_LENGTH);
@@ -26,8 +26,8 @@ impl MessageToMainnet {
         Self {
             recipient: bytes[0..20].into(),
             value: U256::from_big_endian(&bytes[20..52]),
-            sidenet_transaction_hash: bytes[52..84].into(),
-            mainnet_gas_price: U256::from_big_endian(&bytes[84..MESSAGE_LENGTH]),
+            side_transaction_hash: bytes[52..84].into(),
+            main_gas_price: U256::from_big_endian(&bytes[84..MESSAGE_LENGTH]),
         }
     }
 
@@ -44,8 +44,8 @@ impl MessageToMainnet {
         Ok(Self {
             recipient: withdraw_log.recipient,
             value: withdraw_log.value,
-            sidenet_transaction_hash: hash,
-            mainnet_gas_price: withdraw_log.home_gas_price,
+            side_transaction_hash: hash,
+            main_gas_price: withdraw_log.home_gas_price,
         })
     }
 
@@ -56,8 +56,8 @@ impl MessageToMainnet {
         let mut result = vec![0u8; MESSAGE_LENGTH];
         result[0..20].copy_from_slice(&self.recipient.0[..]);
         self.value.to_big_endian(&mut result[20..52]);
-        result[52..84].copy_from_slice(&self.sidenet_transaction_hash.0[..]);
-        self.mainnet_gas_price
+        result[52..84].copy_from_slice(&self.side_transaction_hash.0[..]);
+        self.main_gas_price
             .to_big_endian(&mut result[84..MESSAGE_LENGTH]);
         return result;
     }
@@ -75,54 +75,54 @@ mod test {
     use rustc_hex::FromHex;
 
     #[test]
-    fn test_message_to_mainnet_to_bytes() {
+    fn test_message_to_main_to_bytes() {
         let recipient: Address = "0xeac4a655451e159313c3641e29824e77d6fcb0ce".into();
         let value = U256::from_dec_str("3800000000000000").unwrap();
-        let sidenet_transaction_hash: H256 =
+        let side_transaction_hash: H256 =
             "0x75ebc3036b5a5a758be9a8c0e6f6ed8d46c640dda39845de99d9570ba76798e2".into();
-        let mainnet_gas_price = U256::from_dec_str("8000000000").unwrap();
+        let main_gas_price = U256::from_dec_str("8000000000").unwrap();
 
-        let message = MessageToMainnet {
+        let message = MessageToMain {
             recipient,
             value,
-            sidenet_transaction_hash,
-            mainnet_gas_price,
+            side_transaction_hash,
+            main_gas_price,
         };
 
         assert_eq!(message.to_bytes(), "eac4a655451e159313c3641e29824e77d6fcb0ce000000000000000000000000000000000000000000000000000d80147225800075ebc3036b5a5a758be9a8c0e6f6ed8d46c640dda39845de99d9570ba76798e200000000000000000000000000000000000000000000000000000001dcd65000".from_hex().unwrap())
     }
 
     quickcheck! {
-        fn quickcheck_message_to_mainnet_roundtrips_to_bytes(
+        fn quickcheck_message_to_main_roundtrips_to_bytes(
             recipient_raw: Vec<u8>,
             value_raw: u64,
-            sidenet_transaction_hash_raw: Vec<u8>,
-            mainnet_gas_price_raw: u64
+            side_transaction_hash_raw: Vec<u8>,
+            main_gas_price_raw: u64
         ) -> TestResult {
-            if recipient_raw.len() != 20 || sidenet_transaction_hash_raw.len() != 32 {
+            if recipient_raw.len() != 20 || side_transaction_hash_raw.len() != 32 {
                 return TestResult::discard();
             }
 
             let recipient: Address = recipient_raw.as_slice().into();
             let value: U256 = value_raw.into();
-            let sidenet_transaction_hash: H256 = sidenet_transaction_hash_raw.as_slice().into();
-            let mainnet_gas_price: U256 = mainnet_gas_price_raw.into();
+            let side_transaction_hash: H256 = side_transaction_hash_raw.as_slice().into();
+            let main_gas_price: U256 = main_gas_price_raw.into();
 
-            let message = MessageToMainnet {
+            let message = MessageToMain {
                 recipient,
                 value,
-                sidenet_transaction_hash,
-                mainnet_gas_price
+                side_transaction_hash,
+                main_gas_price
             };
 
             let bytes = message.to_bytes();
-            assert_eq!(message, MessageToMainnet::from_bytes(bytes.as_slice()));
+            assert_eq!(message, MessageToMain::from_bytes(bytes.as_slice()));
 
             let payload = message.to_payload();
             let mut tokens = ethabi::decode(&[ethabi::ParamType::Bytes], payload.as_slice())
                 .unwrap();
             let decoded = tokens.pop().unwrap().to_bytes().unwrap();
-            assert_eq!(message, MessageToMainnet::from_bytes(decoded.as_slice()));
+            assert_eq!(message, MessageToMain::from_bytes(decoded.as_slice()));
 
             TestResult::passed()
         }
