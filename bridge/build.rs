2a8623a44d77d5560fd8b4ee2b48fd4134f27775
Ayushya Chitransh <ayushyachitransh@gmail.com>
2018-05-07 13:17:05 +0530

    Formatted code according to cargo-fmt

--- bridge/build.rs
+++ bridge/build.rs
@@ -34,7 +34,7 @@ fn main() {
                 } else {
                     panic!("`solc` exited because it was terminated by a signal");
                 }
-            } else{
+            } else {
                 let output = Command::new("solc").args(&["--version"]).output().unwrap();
                 let output_string = String::from_utf8(output.stdout).unwrap();
                 let solc_version = output_string.lines().last().unwrap();
@@ -49,7 +49,7 @@ fn main() {
                         let solc_version = output_string.lines().last().unwrap();
                         println!("cargo:rustc-env=SOLC_VERSION={}", solc_version);
 
-                        match fs::remove_dir_all("../compiled_contracts"){
+                        match fs::remove_dir_all("../compiled_contracts") {
                             Ok(()) => {
                                 println!("Removed old files");
                             }
@@ -66,40 +66,52 @@ fn main() {
                             .arg("../compiled_contracts")
                             .arg("../contracts/bridge.sol")
                             .status()
-                            {
-                                Ok(exit_status) => {
-                                    if !exit_status.success() {
-                                        if let Some(code) = exit_status.code() {
-                                            panic!("`solc` exited with error exit status code `{}`", code);
-                                        } else {
-                                            panic!("`solc` exited because it was terminated by a signal");
-                                        }
+                        {
+                            Ok(exit_status) => {
+                                if !exit_status.success() {
+                                    if let Some(code) = exit_status.code() {
+                                        panic!(
+                                            "`solc` exited with error exit status code `{}`",
+                                            code
+                                        );
                                     } else {
-                                        // make solc version used to compile contracts (`solc --version`)
-                                        // available via `env!("SOLC_VERSION")` in sources
-                                        let output = Command::new("solcjs").args(&["--version"]).output().unwrap();
-                                        let output_string = String::from_utf8(output.stdout).unwrap();
-                                        let solc_version = output_string.lines().last().unwrap();
-                                        println!("cargo:rustc-env=SOLC_VERSION={}", solc_version);
+                                        panic!(
+                                            "`solc` exited because it was terminated by a signal"
+                                        );
                                     }
-                                }
-                                Err(err) => {
-                                    if let std::io::ErrorKind::NotFound = err.kind() {
-                                        panic!("`solc` executable not found in `$PATH`. `solc` is required to compile the bridge contracts. please install it: https://solidity.readthedocs.io/en/develop/installing-solidity.html");
-                                    } else {
-                                        panic!("an error occurred when trying to spawn `solc`: {}", err);
-                                    }
+                                } else {
+                                    // make solc version used to compile contracts (`solc --version`)
+                                    // available via `env!("SOLC_VERSION")` in sources
+                                    let output = Command::new("solcjs")
+                                        .args(&["--version"])
+                                        .output()
+                                        .unwrap();
+                                    let output_string = String::from_utf8(output.stdout).unwrap();
+                                    let solc_version = output_string.lines().last().unwrap();
+                                    println!("cargo:rustc-env=SOLC_VERSION={}", solc_version);
                                 }
                             }
-                            let prepend = "../compiled_contracts/";
-                            let paths = fs::read_dir("../compiled_contracts").unwrap();
-                            for path in paths {
-                            let _file_name: String = format!("{}{}",prepend, get_file_name(path.unwrap()));
-                            let _replaced_name : String = str::replace(&_file_name, "___contracts_bridge_sol_", "");
-                            match fs::rename(&_file_name, &_replaced_name){
+                            Err(err) => {
+                                if let std::io::ErrorKind::NotFound = err.kind() {
+                                    panic!("`solc` executable not found in `$PATH`. `solc` is required to compile the bridge contracts. please install it: https://solidity.readthedocs.io/en/develop/installing-solidity.html");
+                                } else {
+                                    panic!(
+                                        "an error occurred when trying to spawn `solc`: {}",
+                                        err
+                                    );
+                                }
+                            }
+                        }
+                        let prepend = "../compiled_contracts/";
+                        let paths = fs::read_dir("../compiled_contracts").unwrap();
+                        for path in paths {
+                            let _file_name: String =
+                                format!("{}{}", prepend, get_file_name(path.unwrap()));
+                            let _replaced_name: String =
+                                str::replace(&_file_name, "___contracts_bridge_sol_", "");
+                            match fs::rename(&_file_name, &_replaced_name) {
                                 Ok(_status) => {
                                     println!("Renamed successfully");
-
                                 }
                                 Err(err) => {
                                     println!("Tried looking for {}", &_file_name);
@@ -117,7 +129,6 @@ fn main() {
                         }
                     }
                 }
-
             } else {
                 panic!("an error occurred when trying to spawn `solc`: {}", err);
             }
@@ -125,10 +136,9 @@ fn main() {
     }
 }
 fn get_file_name(path: DirEntry) -> String {
-    let file_name : String = path
-        .file_name()
-        .into_string()
-        .expect("error: the first argument is not a file \
-                    system path representable in UTF-8.");
+    let file_name: String = path.file_name().into_string().expect(
+        "error: the first argument is not a file\
+         system path representable in UTF-8.",
+    );
     file_name
 }
