01790fd20e12e1f4a65164abd12a75ed82c5dd84
Maximilian Krüger <kruemaxi@googlemail.com>
2018-05-15 22:48:22 +0200

    bridge/src/bridge/withdraw_confirm.rs -> bridge/src/side_to_main_sign.rs

--- bridge/src/side_to_main_sign.rs
+++ bridge/src/side_to_main_sign.rs
@@ -9,47 +9,32 @@ use web3::types::{Address, Bytes, H256, H520, Log, U256};
 use log_stream::LogStream;
 use contracts::foreign::ForeignBridge;
 use error::{self, ResultExt};
-use message_to_mainnet::{MessageToMainnet, MESSAGE_LENGTH};
-use contract_connection::ContractConnection;
+use message_to_main::{MessageToMain, MESSAGE_LENGTH};
 use web3::helpers::CallResult;
-use relay_stream::RelayFactory;
+use relay_stream::LogToFuture;
+use side_contract::{SideContract, IsSideToMainSignedOnSide};
 
 enum State<T: Transport> {
+    AwaitCallHasSubmittedSignature(Timeout<IsSideToMainSignedOnSide<T>>),
     AwaitSignature(Timeout<FromErr<CallResult<H520, T::Out>, error::Error>>),
     AwaitTransaction(Timeout<FromErr<CallResult<H256, T::Out>, error::Error>>),
 }
 
 pub struct SideToMainSign<T: Transport> {
     tx_hash: H256,
-    options: Options<T>,
-    message: MessageToMainnet,
+    side: SideContract<T>,
+    message: MessageToMain,
     state: State<T>,
 }
 
-#[derive(Clone)]
-pub struct Options<T: Transport> {
-    pub gas: U256,
-    pub gas_price: U256,
-    pub address: Address,
-    pub side: ContractConnection<T>,
-}
-
-/// from the options and a log a relay future can be made
-impl<T: Transport> RelayFactory for Options<T> {
-    type Relay = SideToMainSign<T>;
-
-    fn log_to_relay(&self, log: Log) -> Self::Relay {
-        SideToMainSign::new(log, self.clone())
-    }
-}
 
 impl<T: Transport> SideToMainSign<T> {
-    pub fn new(log: Log, options: Options<T>) -> Self {
+    pub fn new(log: Log, side: SideContract<T>) -> Self {
         let tx_hash = log.transaction_hash
             .expect("`log` must be mined and contain `transaction_hash`. q.e.d.");
 
         let message =
-            MessageToMainnet::from_log(log).expect("`log` must contain valid message. q.e.d.");
+            MessageToMain::from_log(log).expect("`log` must contain valid message. q.e.d.");
         let message_bytes = message.to_bytes();
 
         assert_eq!(
@@ -59,12 +44,12 @@ impl<T: Transport> SideToMainSign<T> {
             MESSAGE_LENGTH
         );
 
-        let future = options.side.sign(Bytes(message_bytes));
+        let future = side.sign(Bytes(message_bytes));
         let state = State::AwaitSignature(future);
         info!("{:?} - step 1/3 - about to sign message", tx_hash);
 
         Self {
-            options,
+            side,
             tx_hash,
             message,
             state,
@@ -91,16 +76,6 @@ impl<T: Transport> Future for SideToMainSign<T> {
                         self.tx_hash
                     );
 
-                    let payload = ForeignBridge::default()
-                        .functions()
-                        .submit_signature()
-                        .input(signature.0.to_vec(), self.message.to_bytes());
-
-                    let future = self.options.side.send_transaction(
-                        Bytes(payload),
-                        self.options.gas,
-                        self.options.gas_price,
-                    );
                     State::AwaitTransaction(future)
                 }
                 State::AwaitTransaction(ref mut future) => {
@@ -118,5 +93,18 @@ impl<T: Transport> Future for SideToMainSign<T> {
             };
             self.state = next_state;
         }
+    }
+}
+
+pub struct LogToSideToMainSign<T: Transport> {
+    pub side: SideContract<T>
+}
+
+/// from the options and a log a relay future can be made
+impl<T: Transport> LogToFuture for LogToSideToMainSign<T> {
+    type Future = SideToMainSign<T>;
+
+    fn log_to_future(&self, log: Log) -> Self::Future {
+        SideToMainSign::new(log, self.side.clone())
     }
 }
