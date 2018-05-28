36b491b46f5b090c5d99bf9cb5d66f29b791ee5e
Maximilian Krüger <kruemaxi@googlemail.com>
2018-05-28 13:56:45 +0200

    lib.rs: update modules

--- bridge/src/lib.rs
+++ bridge/src/lib.rs
@@ -31,13 +31,19 @@ mod macros;
 #[macro_use]
 mod test;
 
-pub mod contract_connection;
 pub mod config;
 pub mod bridge;
 pub mod database;
 pub mod error;
 pub mod relay_stream;
+pub mod side_contract;
+pub mod main_contract;
+pub mod deploy;
+pub mod main_to_side_sign;
+pub mod side_to_main_sign;
+pub mod side_to_main_signatures;
 pub mod helpers;
+pub mod future_heap;
 
 mod log_stream;
 pub use log_stream::{LogStream, LogStreamOptions};
@@ -45,8 +51,8 @@ pub use log_stream::{LogStream, LogStreamOptions};
 mod signature;
 pub use signature::Signature;
 
-mod message_to_mainnet;
-pub use message_to_mainnet::{MessageToMainnet, MESSAGE_LENGTH};
+mod message_to_main;
+pub use message_to_main::{MessageToMain, MESSAGE_LENGTH};
 
 #[cfg(test)]
 extern crate jsonrpc_core;
