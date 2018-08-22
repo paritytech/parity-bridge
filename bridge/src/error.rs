18781e5b548a6d2686e26a776fde1025d47f1684
Maximilian Krüger <kruemaxi@googlemail.com>
2018-08-22 22:31:24 +0200

    error.rs: removed #![allow(unknown_lints)]

--- bridge/src/error.rs
+++ bridge/src/error.rs
@@ -13,7 +13,6 @@
 
 // You should have received a copy of the GNU General Public License
 // along with Parity-Bridge.  If not, see <http://www.gnu.org/licenses/>.
-#![allow(unknown_lints)]
 
 //! error chain
 
